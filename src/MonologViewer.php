6a5082e40cc0f406fefc8ada752d0acfdd6f8689
davtur19 <dav.tur19@gmail.com>
2024-05-28 19:35:54 +0200

    Add files via upload

--- src/MonologViewer.php
+++ src/MonologViewer.php
@@ -1,7 +1,12 @@
 <?php
-namespace Jtclark;
+
+namespace davtur19;
+
 use Exception;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 use Twig\Extension\CoreExtension;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -10,10 +15,12 @@ use Westsworld\TimeAgo;
 
 class MonologViewer
 {
-    protected $settings = [];
+    protected array $settings = [];
 
-    public function __construct(array $settings)
-    {
+    /**
+     * @throws Exception
+     */
+    public function __construct(array $settings) {
         if (empty($settings['user'])) {
             throw new Exception('user must be set');
         }
@@ -30,8 +37,7 @@ class MonologViewer
     /**
      * Very basic HTTP authentication
      */
-    public function authenticate()
-    {
+    public function authenticate(): void {
         if (!isset($_SERVER['PHP_AUTH_USER'])) {
             header('WWW-Authenticate: Basic realm="My Realm"');
             header('HTTP/1.0 401 Unauthorized');
@@ -39,8 +45,7 @@ class MonologViewer
             exit;
         }
 
-        if ($_SERVER['PHP_AUTH_USER'] != $this->settings['user'] ||
-            $_SERVER['PHP_AUTH_PW'] != $this->settings['pass']) {
+        if ($_SERVER['PHP_AUTH_USER'] != $this->settings['user'] || $_SERVER['PHP_AUTH_PW'] != $this->settings['pass']) {
             header('WWW-Authenticate: Basic realm="My Realm"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Invalid credentials';
@@ -52,14 +57,16 @@ class MonologViewer
      * Filters the logs and renders them
      *
      * @param int $lines Number of lines to return, if null, entire file will be read
-     * @param string $logLevelFilter log level type to filter, debug, critical, error, warning, info
-     * @param string $supportCode
-     * @param string $search
-     * @throws Exception
+     * @param string|null $logLevelFilter log level type to filter, debug, critical, error, warning, info
+     * @param string|null $supportCode
+     * @param string|null $search
+     * @param null $extra
      * @return void
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function render($lines = 100, $logLevelFilter = null, $supportCode = null, $search = null)
-    {
+    public function render(int $lines = 100, string $logLevelFilter = null, string $supportCode = null, string $search = null, $extra = null): void {
         $logPath = $this->settings['path'];
 
         if ($logLevelFilter === 'all') {
@@ -71,138 +78,147 @@ class MonologViewer
             die('Log file does not exist');
         }
 
-        $loader = new FilesystemLoader(__DIR__ . '/templates');
-        $twig = new Environment($loader, [
-            'debug' => true,
+        $templates_dir = !empty($this->settings['templates_dir']) ? $this->settings['templates_dir'] : __DIR__ . '/templates';
+        $loader = new FilesystemLoader($templates_dir);
+        $twig = new Environment($loader, [//'debug' => true,
         ]);
 
-        $twig->addFilter(new TwigFilter('ago', function ($dateTime) {
+        $twig->addFilter(new TwigFilter('ago', function ($dateTime)
+        {
             $timeAgo = new TimeAgo();
             return $timeAgo->inWordsFromStrings($dateTime);
         }));
 
-        $twig->addFilter(new TwigFilter('alertIconClass', function ($string) {
-            switch ($string) {
-                case 'ERROR':
-                case 'CRITICAL':
-                    return 'fa-exclamation-circle';
-                    break;
-                case 'WARNING':
-                    return 'fa-exclamation-triangle';
-                    break;
-                case 'INFO':
-                case 'DEBUG':
-                    return 'fa-info-circle';
-                    break;
-            }
-            return '';
+        $twig->addFilter(new TwigFilter('alertIconClass', function ($string)
+        {
+            return match ($string) {
+                'ERROR', 'CRITICAL' => 'fa-exclamation-circle',
+                'WARNING'           => 'fa-exclamation-triangle',
+                'INFO', 'DEBUG'     => 'fa-info-circle',
+                default             => '',
+            };
         }));
 
-        $twig->addFilter(new TwigFilter('alertClass', function ($string) {
-            switch ($string) {
-                case 'ERROR':
-                case 'CRITICAL':
-                    return 'alert-danger';
-                    break;
-                case 'WARNING':
-                    return 'alert-warning';
-                    break;
-                case 'INFO':
-                case 'DEBUG':
-                    return 'alert-info';
-                    break;
-            }
-            return '';
+        $twig->addFilter(new TwigFilter('alertClass', function ($string)
+        {
+            return match ($string) {
+                'ERROR', 'CRITICAL' => 'alert-danger',
+                'WARNING'           => 'alert-warning',
+                'INFO', 'DEBUG'     => 'alert-info',
+                default             => '',
+            };
         }));
 
-        if ($lines === null) {
-            $lines = array_reverse(explode("\n", file_get_contents($logPath)));
+        $lines_read = array_reverse(explode("\n", file_get_contents($logPath)));
+        // tail è una funzione lentissima, si fa molto prima a leggere tutto il file...
+        /*if ($lines === null) {
+            $lines_read = array_reverse(explode("\n", file_get_contents($logPath)));
         } else {
-            $lines = array_reverse(explode("\n", $this->tail($logPath, $lines)));
-        }
+            $lines_read = array_reverse(explode("\n", $this->tail($logPath, $lines)));
+        }*/
+        $n_lines = 0;
         $logs = [];
-        foreach ($lines as $line) {
+        foreach ($lines_read as $line) {
+            if ($n_lines == $lines) {
+                break;
+            }
+
             if (!$line) {
                 continue;
             }
+
+            $line = trim($line);
+
             $json = json_decode($line, true);
             if ($json === null) {
+                echo json_last_error_msg() . PHP_EOL;
                 die('Could not read log line: ' . $line);
             }
 
-            if (strtolower($logLevelFilter) && strtolower($json['level_name']) != strtolower($logLevelFilter)) {
+            if (strtolower($logLevelFilter ?? '') && strtolower($json['level_name']) != strtolower($logLevelFilter ?? '')) {
                 continue;
             }
             if ($supportCode && $json['extra']['uid'] != $supportCode) {
                 continue;
             }
-            if ($search && strpos($line, $search) === false) {
+            if ($search && !str_contains($line, $search)) {
                 continue;
             }
 
+            $n_lines++;
             $logs[] = $json;
-
         }
+
+        $context = [
+            'logs'   => $logs,
+            'search' => [
+                'sc'     => $supportCode,
+                'filter' => $logLevelFilter,
+                'q'      => $search,
+                'lines'  => $lines
+            ]
+        ];
+        // setto extra per usare l'interfaccia correttamente
+        if (isset($extra['beta'])) {
+            $context['search']['beta'] = $extra['beta'];
+        }
+        if (isset($extra['day'])) {
+            $context['search']['day'] = $extra['day'];
+        }
+
         //var_export($logs);
         $template = !empty($this->settings['template']) ? $this->settings['template'] : 'log.twig';
-        echo $twig->render($template, [
-            'logs' => $logs,
-            'search' => [
-                'sc' => $supportCode,
-                'filter' => $logLevelFilter,
-                'q' => $search
-            ]
-        ]);
+        echo $twig->render($template, $context);
     }
 
     /**
      * Read X lines using a dynamic buffer (more efficient for all file sizes)
      *
+     * @param           $filepath
+     * @param int $lines
+     * @param bool $adaptive
+     * @return bool|string
      * @author Lorenzo Stanco
      * @url https://gist.github.com/lorenzos/1711e81a9162320fde20
      *
-     * @param $filepath
-     * @param int $lines
-     * @param bool|true $adaptive
-     * @return bool|string
      */
-    public function tail($filepath, $lines = 100, $adaptive = true)
-    {
+    public function tail($filepath, int $lines = 100, bool $adaptive = true): bool|string {
 
         // Open file
         $f = @fopen($filepath, "rb");
-        if ($f === false) return false;
+        if ($f === false) {
+            return false;
+        }
 
         // Sets buffer size
-        if (!$adaptive) $buffer = 4096;
-        else $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+        if (!$adaptive) {
+            $buffer = 4096;
+        } else {
+            $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+        }
 
         // Jump to last character
         fseek($f, -1, SEEK_END);
 
         // Read it and adjust line number if necessary
         // (Otherwise the result would be wrong if file doesn't end with a blank line)
-        if (fread($f, 1) != "\n") $lines -= 1;
+        if (fread($f, 1) != "\n") {
+            $lines -= 1;
+        }
 
         // Start reading
         $output = '';
-        $chunk = '';
 
         // While we would like more
         while (ftell($f) > 0 && $lines >= 0) {
-
             // Figure out how far back we should jump
             $seek = min(ftell($f), $buffer);
-
             // Do the jump (backwards, relative to where we are)
             fseek($f, -$seek, SEEK_CUR);
-
             // Read a chunk and prepend it to our output
             $output = ($chunk = fread($f, $seek)) . $output;
-
             // Jump back to where we started reading
             fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
-
             // Decrease our line counter
             $lines -= substr_count($chunk, "\n");
 
@@ -211,10 +227,8 @@ class MonologViewer
         // While we have too many lines
         // (Because of buffer size we might have read too many)
         while ($lines++ < 0) {
-
             // Find first newline and remove all text before that
             $output = substr($output, strpos($output, "\n") + 1);
-
         }
 
         // Close file and return
